bce5528f28a5611986ee740a512315334a36982c
ycy <app97204@gmail.com>
2022-06-07 09:25:19 +0800

    change http get to post

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "01d1b423f870cc1daaadcf14f917fe4b",
 "icons/Icon-maskable-192.png": "5c7b9e86ba646519a960836f1376d5c5",
 "icons/Icon-maskable-512.png": "01d1b423f870cc1daaadcf14f917fe4b",
-"index.html": "e2223495dc34380c202ffca4e0c80756",
-"/": "e2223495dc34380c202ffca4e0c80756",
-"main.dart.js": "02d11120f4f27c6e26a9adb0a7dbc2a1",
+"index.html": "4f41fbabf3722b54a220da755e391063",
+"/": "4f41fbabf3722b54a220da755e391063",
+"main.dart.js": "00d80a7bfc348c146b18a519eab46b0c",
 "manifest.json": "0864b7661f3125e720fc6d2b55a6b101",
 "version.json": "e41171e693a91cd732eff50e0f872da5"
 };
