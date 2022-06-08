7b5349e7bf99be50afcff3dd813ab8c53cfa05f5
ycy <app97204@gmail.com>
2022-06-08 21:21:45 +0800

    add color picker

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,7 +8,7 @@ const RESOURCES = {
 "assets/assets/scan.png": "f4fe67bac1fe43eee55242e85273055b",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/NOTICES": "714b0786628e525f203c1c074fcadafa",
+"assets/NOTICES": "c930942b052cc936d06945f651d1bba0",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "01d1b423f870cc1daaadcf14f917fe4b",
 "icons/Icon-maskable-192.png": "5c7b9e86ba646519a960836f1376d5c5",
 "icons/Icon-maskable-512.png": "01d1b423f870cc1daaadcf14f917fe4b",
-"index.html": "37b547815fd97567da256ef5d24b4da6",
-"/": "37b547815fd97567da256ef5d24b4da6",
-"main.dart.js": "67540e1f7585b2ff0cbd66f391041d2b",
+"index.html": "6fd820b8a2c12ba661d78413a6e96eb2",
+"/": "6fd820b8a2c12ba661d78413a6e96eb2",
+"main.dart.js": "ba9fc4622ba2ccb77cd6a2df57b83e31",
 "manifest.json": "0864b7661f3125e720fc6d2b55a6b101",
 "version.json": "e41171e693a91cd732eff50e0f872da5"
 };
